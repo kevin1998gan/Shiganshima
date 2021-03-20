c1ace08d70889a6f1cbf6ebf9613a5d14834b429
WeiYan <weiyan1289@gmail.com>
2021-03-21 03:03:04 +0800

    minor change on css and remove update-form required(admin)

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,15 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>shiganshina clothes | Admin</title>
 
-    <!-- Imports
-    =========================================== -->
+    <!-- Imports -->
     <link rel="stylesheet" href="/css/admin.css">
 </head>
 
 <body>
 
-    <!-- Landing section
-    =========================================== -->
+    <!-- Landing section -->
     <div class="partial-landing">
         <div class="logo">
             <div class="logo-up">シガンシナ</div>
@@ -23,8 +21,7 @@
         </div>
         <div class="user" title="whoami">Welcome Back {{ $user['name'] }}!</div>
 
-        <!-- Navbar
-        =========================================== -->
+        <!-- Navbar -->
         <div class="navigate">
             <div class="add" onclick="location.href='#here'">Add</div>
             <div class="edit"><a href="/action">Edit</a></div>
@@ -42,8 +39,7 @@
         <div class="admin">Administrator</div>
     </div>
 
-    <!-- Some dummy lines under the landing section
-    =========================================== -->
+    <!-- Some dummy lines under the landing section -->
     <main>
         <div class="welcom">
             <marquee>Add Some New Item to the Website Here!</marquee>
@@ -54,15 +50,9 @@
     </main>
 
 
-    <!-- Add a record through here
-    =========================================== -->
+    <!-- Add a record through here -->
     <div class="hidden" id="here">
         <h1>Add an Item, Administrator</h1>
-
-
-
-
-
 
         <form name="itemForm" action="addItem" method="POST" class="insert-form" onsubmit="">
             @csrf
@@ -70,71 +60,78 @@
                 <tr>
                     <td>Item Name:</td>
                     <td><input type="text" name="name" id="name" value="{{ old('name') }}"> <br>
-                        <span style="color: red">@error('name'){{ $message }}@enderror</span>
-                        </td>
-
-                    </tr>
-                    <tr>
-                        <td>Price:</td>
-                        <td><input type="text" name="price" id="price" value="{{ old('price') }}"> <br>
-                            <span style="color: red">@error('price'){{ $message }}@enderror</span>
-                            </td>
-
-                        </tr>
-                        <tr>
-                            <td>Photo(In URL):</td>
-                            <td><input type="text" name="image" id="image" value="{{ old('image') }}"> <br>
-                                <span style="color: red">@error('image'){{ $message }}@enderror</span>
-                                </td>
-
-                            </tr>
-                            <tr>
-                                <td>Quantity:</td>
-                                <td><input type="number" name="quantity" id="quantity" value="{{ old('quantity') }}"> <br>
-                                    <span style="color: red">@error('quantity'){{ $message }}@enderror</span>
-                                    </td>
-
-                                </tr>
-                                <tr>
-                                    <td>Category:</td>
-                                    <td><select id="category" name="category" required>
-                                            <option value="Men" {{ old('category') == 'Men' ? 'selected' : '' }} selected>Men
-                                            </option>
-                                            <option value="Women" {{ old('category') == 'Women' ? 'selected' : '' }}>Women</option>
-                                            <option value="Kids" {{ old('category') == 'Kids' ? 'selected' : '' }}>Kids</option>
-                                        </select>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Description:</td>
-                                    <td><textarea name="description" id="description" value="{{ old('description') }}"></textarea>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td><input type="submit" value="Submit" id="submit" name="submit"></td>
-                                </tr>
-                            </table>
-                        </form>
-
-                        <!-- Some background ornaments
-                                                                                                            =========================================== -->
-                        <div class="coolthing1">シガンシナ</div>
-                        <div class="imcircle"></div>
-                        <div class="gradientboost">shiganshina clothes</div>
-                    </div>
-
-                    <!-- Footer
-                                                                                                        =========================================== -->
-                    <footer>
-                        <div class="gibber">
-                            &copy; 2018-2021 Weiyan Holdings Pte Ltd. All rights reserved.
+                        <div class="err-container">
+                            <span style="color: red">@error('name'){{ $message }}@enderror</span>
                         </div>
-                    </footer>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td>Price:</td>
+                    <td><input type="text" name="price" id="price" value="{{ old('price') }}"> <br>
+                        <div class="err-container">
+                            <span style="color: red">@error('price'){{ $message }}@enderror</span>
+                        </div>
+                    </td>
+                </tr>
+                        
+                <tr>
+                    <td>Photo(In URL):</td>
+                    <td><input type="text" name="image" id="image" value="{{ old('image') }}"> <br>
+                        <div class="err-container">
+                            <span style="color: red">@error('image'){{ $message }}@enderror</span>
+                        </div>
+                    </td>
+                </tr>
+                            
+                <tr>
+                    <td>Quantity:</td>
+                    <td><input type="number" name="quantity" id="quantity" value="{{ old('quantity') }}"> <br>
+                        <div class="err-container">
+                            <span style="color: red">@error('quantity'){{ $message }}@enderror</span>
+                        </div>
+                    </td>
+                </tr>
+                                
+                <tr>
+                    <td>Category:</td>
+                    <td>
+                        <select id="category" name="category" required>
+                            <option value="Men" {{ old('category') == 'Men' ? 'selected' : '' }} selected>Men</option>
+                            <option value="Women" {{ old('category') == 'Women' ? 'selected' : '' }}>Women</option>
+                            <option value="Kids" {{ old('category') == 'Kids' ? 'selected' : '' }}>Kids</option>
+                    </select>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td>Description:</td>
+                    <td>
+                        <textarea name="description" id="description" value="{{ old('description') }}"></textarea>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td></td>
+                    <td><input type="submit" value="Submit" id="submit" name="submit"></td>
+                </tr>
+            </table>
+        </form>
 
-                    <!-- Javascript import at bottoom
-                                                                                                        =========================================== -->
-                    <script src="/js/today.js"></script>
-                </body>
+        <!-- Some background ornaments -->
+        <div class="coolthing1">シガンシナ</div>
+        <div class="imcircle"></div>
+        <div class="gradientboost">shiganshina clothes</div>
+    </div>
 
-                </html>
+    <!-- Footer -->
+    <footer>
+        <div class="gibber">
+            &copy; 2018-2021 Weiyan Holdings Pte Ltd. All rights reserved.
+        </div>
+    </footer>
+
+    <!-- Javascript import at bottoom -->
+    <script src="/js/today.js"></script>
+</body>
+</html>
