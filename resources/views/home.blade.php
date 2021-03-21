d7edb2f66129c6280afc3285d5eabebced8b3ca9
data-miner00 <data-miner00@users.noreply.github.com>
2021-03-21 22:00:28 +0800

    feat: Added a home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link href="{{ asset('css/global.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/header.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/047746317e.js" defer></script>
+</head>
+<body>
+  <x-header></x-header>
+  <main style="height: 200vh; width: 100%">
+
+  </main>
+  <x-footer></x-footer>
+</body>
+</html>
